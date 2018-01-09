2e9c2373cb2da738d697933e1a4b6262e22aea20
Steven J Raaen <stevenjraaen@Stevens-MacBook-Pro.local>
2018-01-09 17:36:19 -0500

    cosmetics

--- YesInEurope.js
+++ YesInEurope.js
@@ -12,15 +12,20 @@ export default class YesInEurope extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			markedDates: {}
+			daysIn: 0,
+			daysLeft: 90,
+			markedDates: {},
 		}
 	}
 	onDayPress(day) {
 		const markedDates = Object.assign({}, this.state.markedDates)
-	        markedDates[day.dateString]= {selected: true,  color: 'green'}
-    		console.log(markedDates);
+			console.log(this.state.markDates)
+	      markedDates[day.dateString]= {selected: true,  color:  '#000099'}
+    		
     		this.setState({
-    			markedDates: markedDates
+    			markedDates: markedDates,
+    			daysIn: this.state.daysIn + 1,
+    			daysLeft: this.state.daysLeft - 1,
     		})
 	}
 
@@ -30,50 +35,80 @@ export default class YesInEurope extends Component {
     var height = Dimensions.get('window').height;
 	const styles = StyleSheet.create({
 	  container: {
-	  	flexDirection: "row",
-	  	marginTop: 50,
-	  	justifyContent: 'center',
-	  	marginLeft: width * .03,
-	  	marginRight: width * .03,
+		  	flexDirection: "row",
+		  	marginTop: 30,
+		  	justifyContent: 'center',
+		  	marginLeft: width * .03,
+		  	marginRight: width * .03,
+		  	height: height * .65
 	  },
 	  lines: {
-		width: width, 
-		height: height * .04, 
-		alignSelf: 'stretch', 
-		marginTop: 2,  
-		paddingTop: 2,  
-		backgroundColor: '#000099',
-		color: "#FFCC00",
-		fontSize: 14,
-		fontWeight: 'bold',
-		textAlign: 'center',
-		
-	  }
+			width: width, 
+			height: height * .04, 
+			alignSelf: 'stretch', 
+			marginTop: 2,  
+			paddingTop: 2,  
+			backgroundColor: '#000099',
+			color: "#FFCC00",
+			fontSize: 14,
+			fontWeight: 'bold',
+			textAlign: 'center'		
+	  },
+  	   instructions: {
+  	   	flex: 1,
+  	   	alignSelf: 'stretch',
+		  	marginTop: 16,
+		  	marginBottom: 16,
+		  	height: 20
+	  },
+	   days: {
+	   	flex: 1,
+	   	flexDirection: 'row',
+	   	alignSelf: 'stretch',
+		  	marginTop: 20,
+		  	marginBottom: 20,
+		  	height: 40,
+		  	marginLeft: 20,
+		  	width: width,	 	
+	  },
+
 	});
-
-
 		return (
+	<View>
+		<View style = {styles.instructions}>
+			<Text style={{fontSize: 18, fontWeight: 'bold', textAlign: 'center'}}>
+				Select the days you were in Europe
+			</Text>		
+ 		</View>
+ 		<View style={styles.days}>
+ 			<View style={{flex:2, alignContent: 'flex-start'}}>
+ 				<Text style={{fontSize: 16, fontWeight: 'bold'}}>Days Used</Text>
+ 				<Text style={{fontSize: 20, fontWeight: 'bold'}}>{this.state.daysIn}</Text>
+ 			</View>
+ 			<View style={{flex:2, alignContent: 'flex-end'}}>
+ 				<Text style={{fontSize: 16, fontWeight: 'bold'}} >Days Left</Text>
+ 				<Text style={{fontSize: 16, fontWeight: 'bold'}} >{this.state.daysLeft}</Text>
+ 			</View>
+
+ 		</View>
 			<View style={styles.container}>
-			<CalendarList
-  style={{
-    borderWidth: 1,
-    borderColor: 'gray'
-  }}
-     
-  markingType={'period'}
-  current={this.props.navigation.state.params.start}
-  minDate={this.props.navigation.state.params.start}
-  maxDate={this.props.navigation.state.params.today}
-  futureScrollRange={6}
-  onDayPress={(day) => {this.onDayPress(day)}}
-markingType={'period'}
- monthFormat={'MMMM yyyy'}
- markedDates={this.state.markedDates}
-  // Handler which gets executed when visible month changes in calendar. Default = undefined
- 
-/>
+			  <CalendarList
+				style={{
+				borderWidth: 1,
+				borderColor: 'gray'
+				}}
+				current={this.props.navigation.state.params.start}
+				minDate={this.props.navigation.state.params.start}
+				maxDate={this.props.navigation.state.params.today}
+				pastScrollRange={0}
+				futureScrollRange={6}
+				onDayPress={(day) => {this.onDayPress(day)}}
+				markingType={'period'}
+				monthFormat={'MMMM yyyy'}
+				markedDates={this.state.markedDates}
+				/>
 			</View>
+	</View>
 			)
 	}
-	
 }
